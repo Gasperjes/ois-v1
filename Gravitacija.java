1d3e3526ebc58867274615e04db670c1874eaec6
Gasperjes <gasper.jes@gmail.com>
2024-03-03 21:43:47 +0100

    Dodan izracun gravitacije

--- Gravitacija.java
+++ Gravitacija.java
@@ -1,5 +1,12 @@
 public class Gravitacija{
 	public static void main(String[] args){
-		System.out.println("Ois je zakon!");
+		System.out.println("Ois je zakon! Se strinjam...");
 	}
+	public static double izracunGravitacije(double visina){
+		double c = 6.674e-11;
+		double m = 5.972e24;
+		double r = 6.371e6;
+		return (c * m) / Math.pow(r + visina, 2.0);
+	}
+	
 }
