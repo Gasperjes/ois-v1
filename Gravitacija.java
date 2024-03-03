4619fd55237ec21abba0700ae19237564012c887
Gasperjes <gasper.jes@gmail.com>
2024-03-03 22:17:59 +0100

    Koncna verzija

--- Gravitacija.java
+++ Gravitacija.java
@@ -1,6 +1,10 @@
 public class Gravitacija{
 	public static void main(String[] args){
 		System.out.println("Ois je zakon! Predmet je se kr vredi.");
+		izpis(0.0);
+		izpis(10.0);
+		izpis(1000.0);
+		izpis(100000.0);
 	}
 	public static double izracunGravitacije(double visina){
 		double c = 6.674e-11;
@@ -9,7 +13,7 @@ public class Gravitacija{
 		return (c * m) / Math.pow(r + visina, 2.0);
 	}
 	public static void izpis(double visina){
-		System.out.printf("Grav. pospesek na %d km nad. visine je %d m/s^2\n", visina, izracunGravitacije(visina * 1000.0));
+		System.out.printf("Grav. pospesek na %.2f km nad. visine je %.2f m/s^2\n", visina, izracunGravitacije(visina * 1000.0));
 	}
 	
 }
